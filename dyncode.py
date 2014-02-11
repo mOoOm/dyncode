96d34cc575658a4edde961e017e6431c35f3f8b0
mOoOm <mozdop@gmail.com>
2014-02-11 11:31:19 +0900

    Update dyncode.py

--- dyncode.py
+++ dyncode.py
@@ -3,8 +3,7 @@ import sublime, sublime_plugin
 class DynCodeListener(sublime_plugin.EventListener):
 	def on_post_save(self,view):		
 		if view.file_name() :
-			first_line = view.substr(view.line(0)).split(' ')
-			first_line = first_line.replace('\\','/')
+			first_line = view.substr(view.line(0)).replace('\\','/').split(' ')
 			if first_line[0] == '#DYNCODE' :
 				file_name = view.file_name().split('/')[-1]
 				execute = ("curl " + first_line[1] + "/" + file_name + " -X POST --data-binary @" + view.file_name()).split(' ')
